5082e2beae10ca63092db5a9583f16559dec8b49
Shawn Daniels <SGDaniels08@gmail.com>
2020-08-14 22:59:49 -0400

    Static terrarium with character model

--- main.py
+++ main.py
@@ -10,23 +10,43 @@ import pygame
 def print_terrarium():
     pygame.init()
 
-    #World screen
-    world = pygame.display.set_mode((1000, 700))
-    world.fill((172, 221, 231))
+    # simple colors
+    black = (0, 0, 0)
+    white = (255, 255, 255)
 
-    #Terrarium elements
-    sand = pygame.Surface((700, 400))
-    sand.fill((210, 180, 140))
-    glass = pygame.Surface((700, 200))
-    glass.fill((255, 255, 255))
+    # World screen
+    world_size = (1200, 750)
+    world = pygame.display.set_mode(world_size)
+    world_background_color = (172, 221, 231)
+    world.fill(world_background_color)
 
-    #Blit them together
-    world.blit(glass, (100, 50))
-    world.blit(sand, (100, 250))
-    pygame.draw.line(world, (0, 0, 0), (100, 250), (800, 250), 2)
+    # Terrarium elements
+    terrarium_origin_x = 100
+    terrarium_origin_y = 150
+    terrarium_width = 900
+    terrarium_height = 500
+    sand_color = (210, 180, 140)
+    sand = pygame.Surface((terrarium_width, 300))
+    sand.fill(sand_color)
 
-    #Oval for sizing
-    pygame.draw.ellipse(world, (0, 0, 0), (300, 170, 30, 80))
+    glass_color = (255, 255, 255)
+    glass = pygame.Surface((terrarium_width, 200))
+    glass.fill(glass_color)
+
+    # Blit them together
+    sand_origin_x = terrarium_origin_x
+    sand_origin_y = terrarium_origin_y + 200
+    world.blit(glass, (terrarium_origin_x, terrarium_origin_y))
+    world.blit(sand, (sand_origin_x, sand_origin_y))
+    pygame.draw.line(world, black, (sand_origin_x, sand_origin_y),
+                     (sand_origin_x + terrarium_width, sand_origin_y), 2)
+
+    # Oval for sizing
+    person_height = 75
+    person_width = 20
+    person_origin_x = 300
+    person_origin_y = sand_origin_y - person_height
+    pygame.draw.ellipse(world, black, (person_origin_x, person_origin_y, person_width, person_height))
 
     pygame.display.update()
 
