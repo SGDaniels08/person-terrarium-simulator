6341f4086a888acc372a86f8252436b8e2c3c0fe
Shawn Daniels <SGDaniels08@gmail.com>
2020-09-03 12:11:16 -0400

    Stickfigure walks across screen - now collision detection

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def print_terrarium():
     world_size = (1200, 750)
     world = pygame.display.set_mode(world_size)
     world_background_color = (172, 221, 231)
-    world.fill(world_background_color)
 
     # Terrarium elements
     terrarium_origin_x = 100
@@ -28,28 +27,22 @@ def print_terrarium():
     terrarium_height = 500
     sand_color = (210, 180, 140)
     sand = pygame.Surface((terrarium_width, 300))
-    sand.fill(sand_color)
 
     glass_color = (255, 255, 255)
     glass = pygame.Surface((terrarium_width, 200))
-    glass.fill(glass_color)
 
     # Blit them together
     sand_origin_x = terrarium_origin_x
     sand_origin_y = terrarium_origin_y + 200
-    world.blit(glass, (terrarium_origin_x, terrarium_origin_y))
-    world.blit(sand, (sand_origin_x, sand_origin_y))
-    pygame.draw.line(world, black, (sand_origin_x, sand_origin_y),
-                     (sand_origin_x + terrarium_width, sand_origin_y), 2)
 
     # Oval for sizing
-    person_height = 75
+    person_height = 85
     person_width = 20
     person_box = pygame.Surface((person_width, person_height), pygame.SRCALPHA, 32)
     person_origin_x = 300
     person_origin_y = sand_origin_y - person_height
     pygame.draw.ellipse(person_box, black, (0, 0, person_width, person_height))
-    world.blit(person_box, (person_origin_x, person_origin_y))
+    # world.blit(person_box, (person_origin_x, person_origin_y))
 
     # Text
     game_font = pygame.font.SysFont("Lucida Console", 36)
@@ -62,7 +55,7 @@ def print_terrarium():
         image_path = "frame" + str(i) + ".png"
         im = pygame.image.load(image_path)
         walking_frames.append(im)
-        
+
     # Close program box
 
     # Game loop
@@ -77,9 +70,18 @@ def print_terrarium():
                 pygame.quit()
                 sys.exit()
 
+        # Redraw world -- glass transparency?
+        world.fill(world_background_color)
+        sand.fill(sand_color)
+        glass.fill(glass_color)
+        world.blit(glass, (terrarium_origin_x, terrarium_origin_y))
+        world.blit(sand, (sand_origin_x, sand_origin_y))
+        pygame.draw.line(world, black, (sand_origin_x, sand_origin_y),
+                         (sand_origin_x + terrarium_width, sand_origin_y), 2)
+        
         # Animation
-        world.blit(walking_frames[frame_count], (10, 10))
-
+        world.blit(walking_frames[frame_count], (person_origin_x, person_origin_y))
+        person_origin_x += 2
         frame_count += 1
         if frame_count == 12:
             frame_count = 0
